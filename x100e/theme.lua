b29f96ab6137d8b16d5cf67af4e93d8daf8de739
scturtle <scturtle@gmail.com>
2011-10-06 10:02:23 +0800

    config for x100e

---
+++ x100e/theme.lua
@@ -0,0 +1,72 @@
+-- BASICS
+theme = {}
+theme.font          = "WenQuanYi Micro Hei 12"
+
+theme.bg_focus      = "#359cd4"
+theme.bg_normal     = "#333333"
+theme.bg_urgent     = "#fce94f"
+theme.bg_minimize   = "#0067ce"
+
+theme.fg_normal     = "#eeeeee"
+theme.fg_focus      = "#eeeeee"
+theme.fg_urgent     = "#eeeeee"
+theme.fg_minimize   = "#eeeeee"
+
+theme.border_width  = "2"
+theme.border_normal = "#242424"
+theme.border_focus  = "#c0c0c0"
+theme.border_marked = "#c0c0c0"
+
+-- IMAGES
+theme.layout_fairh           = "/usr/share/awesome/themes/archblue/layouts/fairh.png"
+theme.layout_fairv           = "/usr/share/awesome/themes/archblue/layouts/fairv.png"
+theme.layout_floating        = "/usr/share/awesome/themes/archblue/layouts/floating.png"
+theme.layout_magnifier       = "/usr/share/awesome/themes/archblue/layouts/magnifier.png"
+theme.layout_max             = "/usr/share/awesome/themes/archblue/layouts/max.png"
+theme.layout_fullscreen      = "/usr/share/awesome/themes/archblue/layouts/fullscreen.png"
+theme.layout_tilebottom      = "/usr/share/awesome/themes/archblue/layouts/tilebottom.png"
+theme.layout_tileleft        = "/usr/share/awesome/themes/archblue/layouts/tileleft.png"
+theme.layout_tile            = "/usr/share/awesome/themes/archblue/layouts/tile.png"
+theme.layout_tiletop         = "/usr/share/awesome/themes/archblue/layouts/tiletop.png"
+
+theme.awesome_icon           = "/usr/share/awesome/themes/archblue/awesome-icon.png"
+theme.tasklist_floating_icon = "/usr/share/awesome/themes/archblue/layouts/floating.png"
+
+-- from default for now...
+theme.menu_submenu_icon     = "/usr/share/awesome/themes/default/submenu.png"
+theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
+
+-- MISC
+--theme.wallpaper_cmd         = { "awsetbg /usr/share/backgrounds/cosmos/w.jpg" }
+theme.taglist_squares       = "true"
+theme.titlebar_close_button = "true"
+theme.menu_height           = "20"
+theme.menu_width            = "110"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+
+return theme
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
